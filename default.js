1fe18401b4c6a2999d0b39a47ac35aa79e67f9dc
Manish Shetty <mnshty@amazon.com>
2023-12-09 15:10:36 -0800

    more changed

--- default.js
+++ default.js
@@ -7,12 +7,20 @@ var goToFigma = function() {
         if (data.startsWith('https://')) {
             window.location.href = data;
         }
+    }).fail(function() {
+        $('#password-field').css({
+            color: '#e74c3c'
+        });
     });
 };
 
 $(document).on('keyup', '#password-field', function (e) {
     if (e.key === 'Enter' || e.keyCode === 13) {
         goToFigma();
+    } else {
+        $('#password-field').css({
+            color: 'rgba(0,0,0,.87)'
+        });
     }
 });
 
